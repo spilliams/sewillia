23c16b3aa7414f98d53db2932f36b1321c12b1fe
Spencer Williams <s@spencerenglish.com>
2011-08-11 00:59:48 -0400

    whitespace. demo sidebar

--- sidebar.php
+++ sidebar.php
@@ -8,13 +8,19 @@
 
 <div id='sidebar'>
  
- <?php if ( function_exists('dynamic_sidebar')) :
-   dynamic_sidebar('right-menu');
- endif; ?>
+  <?php if ( function_exists('dynamic_sidebar')) :
+    dynamic_sidebar('right-menu');
+  endif; ?>
  
- <?php if ( is_user_logged_in() ) { ?>
-   <div class='light'><a href="<?php echo wp_logout_url(curPageUrl()); ?>">Log Out</a></div>
- <?php } ?>
+  <div class='light'>
+    <div style='float:left'>twitter<br>last.fm<br>facebook</div>
+    <div style='float:left'>8tracks<br>linkedin<br>github</div>
+    <div class='clearfix'></div>
+  </div>
+ 
+  <?php if ( is_user_logged_in() ) { ?>
+    <div class='light'><a href="<?php echo wp_logout_url(curPageUrl()); ?>">Log Out</a></div>
+  <?php } ?>
 </div>
 
 <!-- end sidebar -->
