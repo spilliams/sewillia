e9a7f7b5792f4efaae0c1e16fc4c86dd9cd406ec
Spencer Williams <s@spencerenglish.com>
2011-08-11 12:00:03 -0400

    tweaks and bumps

--- footer.php
+++ footer.php
@@ -16,6 +16,7 @@
         <div class='footer-menu'>and a second one!</div>
         <div class='vr'></div>
         <div class='footer-menu'>and a second second one!</div>
+        <div class='clearfix'></div>
       </div>
     </div>
     <div id='copy'>&copy; 2011 <a href='mailto:s@spencerenglish.com'>Spencer English Williams</a>. Last updated 10 August 2011. <?php echo sprintf(__("Powered by <a href='http://wordpress.org/' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?>.</div>
