68d3b7e6b1447900f3880edf7583e7e960d9828c
Spencer Williams <s@spencerenglish.com>
2011-08-15 19:15:14 -0400

    page formatting

--- page.php
+++ page.php
@@ -4,7 +4,15 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
-<div class='article light two-wide'><?php the_content(); ?></div>
+<div class='article light full-wide'>
+  <div class='header'>
+    <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+  </div>
+  <div class='meta'>
+    Posted on <?php the_time('j F Y') ?> by <?php the_author(); ?>. <?php edit_post_link('Edit', '', ''); ?>
+  </div>
+  <?php the_content(); ?>
+</div>
 <?php endwhile; endif; ?>
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
