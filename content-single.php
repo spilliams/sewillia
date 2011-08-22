d5b1c6f6e56b80992d3c39b63382dce0867df8b6
Spencer Williams <s@spencerenglish.com>
2011-08-22 03:37:04 -0400

    don't link to self

--- content-single.php
+++ content-single.php
@@ -1,9 +1,7 @@
 <?php /* this displays normal format posts */ ?>
 <!-- begin content-single -->
 <div class='article light full-wide'>
-  <div class='header'>
-    <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
-  </div>
+  <div class='header'><?php the_title(); ?></div>
   <div class='meta'>
     Posted on <?php the_time('j F Y') ?> by <?php the_author(); ?>. <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?><?php edit_post_link('Edit', ' | ', ''); ?>
   </div>
