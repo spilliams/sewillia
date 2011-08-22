37fb3c6f07e88d10c6461204bbf0dabdf6edfda0
Spencer Williams <s@spencerenglish.com>
2011-08-22 03:21:18 -0400

    removes commenting

--- content.php
+++ content.php
@@ -5,16 +5,9 @@
     <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
   </div>
   <div class='meta'>
-    Posted on <?php the_time('j F Y') ?> by <?php the_author(); ?>. <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+    Posted on <?php the_time('j F Y') ?> by <?php the_author(); ?>. <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>
   </div>
 
   <?php the_content(__('(more...)')); ?>
-
-  <p>
-    <?php wp_link_pages(); ?>
-    <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)')); ?>
-  </p>
-
-   <?php comments_template(); // Get wp-comments.php template ?>
 </div>
 <!-- end content -->
