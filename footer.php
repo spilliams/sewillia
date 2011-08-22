6716b7506d4ea45f5fa990ca17999b4ace84fae5
Spencer Williams <s@spencerenglish.com>
2011-08-22 03:52:31 -0400

    network updates, unimportant

--- footer.php
+++ footer.php
@@ -28,10 +28,10 @@
         </div>
         <div class='footer-menu one-wide'>
           Network!<br>
-          <a href='http://twitter.com/#!/spilliams' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_37.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_37.png' style='display:none' class='hi'> 29 followers</a><br>
+          <a href='http://twitter.com/#!/spilliams' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_37.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_37.png' style='display:none' class='hi'> 30 followers</a><br>
           <a href='http://github.com/spilliams' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_17.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_17.png' style='display:none' class='hi'> 5 public repos</a><br>
           <a href='http://www.linkedin.com/in/spencerenglish' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_27.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_27.png' style='display:none' class='hi'> 28 connections</a><br>
-          <a href='http://www.last.fm/user/pastavibes' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_25.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_25.png' style='display:none' class='hi'> 29532 plays</a><br>
+          <a href='http://www.last.fm/user/pastavibes' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_25.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_25.png' style='display:none' class='hi'> 29537 plays</a><br>
           <a href='http://www.facebook.com/spilliams' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_09.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_09.png' style='display:none' class='hi'> 119 friends</a><br>
           <a href='https://foursquare.com/spilliams' target='_blank' class='networklink'><img src='/wp-content/themes/sewillia/images/grey_12.png' class='lo'><img src='/wp-content/themes/sewillia/images/three_12.png' style='display:none' class='hi'> 68 checkins</a><br>
         </div>
